3e0192f974d6298efbe68c799c5623b53a02705f
Hamnab02 <hamnab02@gmail.com>
2024-07-16 20:15:58 +0100

    added current location

--- src/index.js
+++ src/index.js
@@ -26,10 +26,13 @@ function updateTime() {
 
 function updateCity(event) {
   let cityTimeZone = event.target.value;
+  if (cityTimeZone === "current") {
+    cityTimeZone = moment.tz.guess();
+  }
   let cityName = cityTimeZone.replace("_", " ").split("/")[1];
   let cityTime = moment().tz(cityTimeZone);
   let citiesElement = document.querySelector("#cities");
-  citiesElement.innerHTML += `
+  citiesElement.innerHTML = `
   <div class="city">
     <div>
       <h2>${cityName}</h2>
@@ -44,7 +47,6 @@ function updateCity(event) {
 
 updateTime();
 setInterval(updateTime, 1000);
-setInterval(updateCity, 1000);
 
 let citiesSelectElement = document.querySelector("#city");
 citiesSelectElement.addEventListener("change", updateCity);
